8e2f1fe31c8a45026b5d03383c97f30fffe13e42
Alex Chernetz <andy80586@gmail.com>
2014-12-09 23:43:15 -0800

    Update versioning and cythonize the setup.py script

--- setup.py
+++ setup.py
@@ -7,51 +7,76 @@
 #
 # This software is licensed as described in the file COPYING, which
 # you should have received as part of this distribution.
-
-import os
 try:
-    import setuptools as setup_mod
+    from setuptools import setup, Extension, find_packages
 except ImportError:
-    import distutils.core as setup_mod
-
-here = os.path.dirname(__file__)
-version = os.path.join(here, 'jsonpickle', 'version.py')
-scope = {}
-exec(open(version).read(), scope)
-
-SETUP_ARGS = dict(
-    name="jsonpickle",
-    version=scope['VERSION'],
-    description="Python library for serializing any "
-                "arbitrary object graph into JSON",
-    long_description =
-        "jsonpickle converts complex Python objects to and "
-        "from JSON.",
-    author="David Aguilar",
-    author_email="davvid -at- gmail.com",
-    url="http://jsonpickle.github.io/",
-    license="BSD",
-    platforms=['POSIX', 'Windows'],
-    keywords=['json pickle', 'json', 'pickle', 'marshal',
-              'serialization', 'JavaScript Object Notation'],
-    classifiers=[
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        'Development Status :: 5 - Production/Stable',
-        "Intended Audience :: Developers",
-        "Programming Language :: Python",
-        "Programming Language :: JavaScript",
-    ],
-    options={'clean': {'all': 1}},
-    packages=["jsonpickle"],
-)
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, Extension, find_packages
+try:
+    from Cython.Distutils import build_ext
+except ImportError:
+    from distutils.command.build_ext import build_ext
 
 
-if __name__ == '__main__':
-    setup_mod.setup(**SETUP_ARGS)
+def get_version():
+    from path import Path
+    from importlib import import_module
+
+    here = Path(__file__).abspath().dirname()
+    version_file = here.joinpath('jsonpickle', 'version.py')
+    version = eval(version_file.lines()[0].split('=')[-1].strip())
+    return version
+
+
+def get_requirements():
+    from path import Path
+
+    here = Path(__file__).abspath().dirname()
+    reqs = here.joinpath('requirements.txt')
+    return reqs.lines()
+
+
+def main():
+    setup(
+        name='jsonpickle',
+        version=get_version(),
+        description='Python library for serializing any arbitrary object graph into JSON',
+        setup_requires=[
+            'Cython>=0.21.1',
+            'path.py>=7.0'
+        ],
+        install_requires=get_requirements(),
+        long_description='jsonpickle converts complex Python objects to and from JSON.',
+        author='David Aguilar',
+        author_email='davvid -at- gmail.com',
+        url='http://jsonpickle.github.io/',
+        license='BSD',
+        platforms=['POSIX', 'Windows'],
+        keywords=['json pickle', 'json', 'pickle', 'marshal',
+                  'serialization', 'JavaScript Object Notation'],
+        classifiers=[
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'Programming Language :: Python',
+            'Programming Language :: JavaScript',
+        ],
+        options={'clean': {'all': 1}},
+        packages=find_packages(exclude=['ez_setup']),
+        include_package_data=True,
+        zip_safe=False,
+        cmdclass={'build_ext': build_ext}
+    )
+
+
+if __name__ == '__main__':  # pragma: no cover
+    main()
+
