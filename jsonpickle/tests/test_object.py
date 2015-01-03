594e66ab391211bcbbe57a35d44f502da74caac5
Alexander Chernetz <alexanderbc@aditi.com>
2015-01-02 16:47:42 -0800

    Fix missing import in jsonpickle.tests.test_object

--- jsonpickle/tests/test_object.py
+++ jsonpickle/tests/test_object.py
@@ -5,6 +5,7 @@ import decimal
 import jsonpickle
 import re
 import sys
+import unittest2
 from unittest2.suite import TestSuite
 from unittest2.loader import makeSuite
 from unittest2.case import TestCase
